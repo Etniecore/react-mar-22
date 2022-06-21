dc37de967acc73ac4cedf17d5096239f3c1a6aae
Etniecore <zavadovskyi.b@gmail.com>
2022-06-21 18:05:59 +0300

    Homework Task 1

---
+++ src/Components/Simpsons.js
@@ -0,0 +1,79 @@
+function Simpsons(){
+    return(
+        <div>
+            <div>
+                <h2>Homer Simpson</h2>
+                <img src="https://static.wikia.nocookie.net/simpsons/images/b/bd/Homer_Simpson.png" alt="Homer"/>
+                <p>Homer Jay Simpson (born May 12, 1956) is the main protagonist of The Simpsons series (or show). He is the spouse of Marge Simpson and father of Bart, Lisa and Maggie Simpson. Homer is overweight (said to be ~240 pounds), lazy, and often ignorant to the world around him. Although Homer has many flaws, he has shown to have great caring, love, and even bravery to those he cares about and, sometimes, even others he doesn't. He also serves as the main protagonist of the The Simpsons Movie.</p>
+                <p> Common phrases</p>
+                <ul>
+                    <li>“D'oh!”
+                        ―Homer when he makes a mistake.</li>
+                    <li>“Why you little...!”
+                        ―Homer strangling or trying to strangle Bart/or someone else.</li>
+                    <li>“Woo-hoo!”
+                        ―Homer when something good happens to him.</li>
+                </ul>
+
+            </div>
+            <div>
+                <h2>Marge Simpson</h2>
+                <img src="https://static.wikia.nocookie.net/simpsons/images/4/4d/MargeSimpson.png" alt="Marge"/>
+                <p>Marjorie Jacqueline "Marge" Simpson  is the homemaker and matriarch of the Simpson family. She is also one of the five main characters in The Simpsons TV series. Marge is 37 years of age. She and her husband Homer have three children: Bart, Lisa, and Maggie. Marge is the moralistic force in her family and often provides a grounding voice in the midst of her family's antics by trying to maintain order in the Simpson household. Aside from her duties at home, Marge has flirted briefly with a number of careers ranging from a police officer to an anti-violence activist.</p>
+                <p>Common phrases</p>
+                <ul>
+                    <li>“Hrmmm….”
+                        ―Marge's catchphrase</li>
+                    <li>“Now it's Marge's time to shine!”
+                        ―Marge Simpson</li>
+                    <li>“Oh, Homie!”
+                        ―Marge's love catchphrase</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Bart Simpson</h2>
+                <img src="https://static.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png" alt="Bart"/>
+                <p>Bartholomew "Bart" Jojo Simpson (born April 1[7] or February 23) is the mischievous, rebellious, misunderstood, disruptive and "potentially dangerous" oldest child of the Simpson family in The Simpsons. He is the only son of Homer and Marge Simpson, and the older brother of Lisa and Maggie. He also has been nicknamed "Cosmo", after discovering a comet in "Bart's Comet". Bart has also been on the cover on numerous comics, such as "Critical Hit", "Simpsons Treasure Trove #11", and "Winter Wingding". Bart also has a 100-issue comic series entitled the Simpson Comics Presents Bart Simpson. Bart is loosely based on Matt Groening and his older brother, Mark Groening.</p>
+                <p>Common phrases</p>
+                <ul>
+                    <li>“Ay Caramba!”
+                        ―Bart's catchphrase and first words</li>
+                    <li>“Eat my shorts!”
+                        ―Bart's second catchphrase</li>
+                    <li>“I didn't do it!”
+                        ―Bart's earlier catchphrase</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Lisa Simpson</h2>
+                <img src="https://static.wikia.nocookie.net/simpsons/images/d/d5/Lisa_Simpson_official.png" alt="Lisa"/>
+                <p>Lisa Marie Simpson (born May 9) is the elder daughter and middle child of the Simpson family and one of the two tritagonists (along with Marge,) of The Simpsons series.</p>
+                <p>Common phrases</p>
+                <ul>
+                    <li>“Bart!”
+                        ―Lisa's first word</li>
+                    <li>“If anyone wants me, I'll be in my room.”
+                        ―Lisa's "catchphrase"</li>
+                    <li>“Quit it, Bart!”
+                        ―Lisa's catchphrase</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Maggie Simpson</h2>
+                <img src="https://static.wikia.nocookie.net/simpsons/images/9/9d/Maggie_Simpson.png" alt="Maggie"/>
+                <p>Margaret Evelyn Lenny "Maggie" Simpson (born January 12, 1989) is the 1-year old daughter and youngest child of Marge and Homer Simpson, the baby sister to Bart and Lisa Simpson and one of the five main characters of The Simpsons. She is often seen sucking on her pacifier, and when she walks, she trips over her clothing and falls on her face. Because she rarely ever talks, Maggie is the least seen and heard in the Simpson family. Maggie is the very young, infant age of 1.</p>
+                <p>Common phrases</p>
+                <ul>
+                    <li>“*suck suck*”
+                        ―Maggie sucking her pacifier</li>
+                    <li>“Good night.”
+                        ―Good Night</li>
+                    <li>“It's your fault I can't talk!”
+                        ―Maggie in Bart's nightmare and her non-canon first words in the series</li>
+                </ul>
+            </div>
+
+
+    </div>)
+}
+export default Simpsons;
