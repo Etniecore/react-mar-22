6ac1f7fb45ae792e8267e8a90939b1c56fc7a9e5
Etniecore <zavadovskyi.b@gmail.com>
2022-06-21 19:07:27 +0300

    Homework 1 Task 2

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -2,9 +2,9 @@ function Characters(props){
     let {id,name,status,species,gender,image} = props;
     return(
         <div>
-            <h3>ID: {id}</h3>
+            <h3> ID: {id}</h3>
             <h2> Name: {name}</h2>
-            <p>Status: {status}</p>
+            <p> Status: {status}</p>
             <p> Species: {species}</p>
             <p> Gender: {gender}</p>
             <img src={image} alt=""/>
