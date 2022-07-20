1ab683480dd6f5e8f3133b452a0e497857fa9384
Etniecore <zavadovskyi.b@gmail.com>
2022-07-20 13:07:15 +0300

    Demo Reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
+
+  let store = useSelector(state=>state); // container like in UseState (i.e: users)
+  let dispatch = useDispatch(); // setter for container as in UseState (i.e: SetUsers)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+            <div>{store}</div>
+      <button onClick={()=>{dispatch({type:'inc', payload:1})}}>Inc</button>
+      <button onClick={()=>{dispatch({type:'dec', payload:1})}}>Dec</button>
+      <button onClick={()=>{dispatch({type:'reset'})}}>Reset</button>
+
     </div>
-  );
+  );// type: it is a type of action we must do// payload it is like how many steps we should do.
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+
+let store = createStore((state = 0, action)=>{ // default state// actions from buttons(i.e: type and payload)
+    switch (action.type){
+        case 'inc':
+            return state + action.payload; // our current state and payload from the dispatch which is linked to certain button
+        case 'dec':
+            return state - action.payload; // our current state and payload from the dispatch which is linked to certain button
+        case 'reset':
+            return 0;
+        default:
+            return state;
+    }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode>
+
+    <Provider store={store}>
+        <App/>
+    </Provider>
+
+
+    </React.StrictMode>)
+;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
